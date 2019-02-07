345cb236f1f768cb9ee0806a2ede5e5fb4249530
mortichar <chrisdayt@gmail.com>
2019-02-06 22:32:14 -0600

    -fPIE flag

--- tool.cpp
+++ tool.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-const string TOOLCHAIN = "./toolchain/bin/arm-linux-androideabi-clang++ -fPIE";
+const string TOOLCHAIN = "./toolchain/bin/arm-linux-androideabi-clang++"
+                         " -march=armv7-a and -mthumb";
 const string TARGET_BASH = "/system/bin/sh";
 const string OPTIONS = "Options: \n"
                         "    1) Execute single command\n"
